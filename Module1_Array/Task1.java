225dfae59f4d114ad9d4ba329398c300655b7df0
241801370029-bit <241801370029@cutmap.ac.in>
2025-12-22 12:06:44 +0530

    Update and rename Task1.java to Module1_Array/Task1.java

---
+++ Module1_Array/Task1.java
@@ -0,0 +1,31 @@
+class Main {
+    public static void main(String[] args) {
+        
+        int[] nums={1,3,4,5,7};
+        int[] num = new int[]{1,2,3};
+        System.out.println(nums[3]);
+        System.out.println(nums.length);
+        System.out.println(nums[1]);
+        System.out.println(nums[2]);
+        
+        for(int i = 0; i < nums.length; i++){
+            System.out.println(nums[i]);
+        }
+            
+            String[] Strings={"raghu","sudha","vyshu"};
+            System.out.println(Strings[0]);
+              System.out.println(Strings[1]);
+                System.out.println(Strings[2]);
+                
+         int[] num1 = new int[10];
+         num1[0]=45;
+         num1[1]=23;
+         num1[5]=9874;
+         num1[9]=495;
+         
+         for(int i = 0; i < num1.length; i++){
+            System.out.println(num1[i]);
+        }
+                   
+    }
+}
